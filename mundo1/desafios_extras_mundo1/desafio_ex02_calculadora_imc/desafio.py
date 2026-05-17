27e5f44d38cfbf23e44f9bdcfe37488334ed52a0
Manoel Gosalan <manoel.gosalan@outlook.com>
2026-05-17 16:36:21 +0100

    feat(desafios_extras_mundo1): solving challenges 1-3

--- mundo1/desafios_extras_mundo1/desafio_ex02_calculadora_imc/desafio.py
+++ mundo1/desafios_extras_mundo1/desafio_ex02_calculadora_imc/desafio.py
@@ -1,4 +1,3 @@
-
 # ============================================================
 # ! DESAFIO EX02 — Calculadora de IMC
 # ============================================================
@@ -10,8 +9,29 @@
 # Descrição:
 # Peça peso e altura e calcule o IMC.
 
-# * ============================================================
-# * ESCREVA SEU CÓDIGO ABAIXO
-# * ============================================================
+titulo    = " Calculadora de IMC ".center(40, "═")
+separador = "─" * 40
+footer    = "═" * 40
 
+altura = float(input("Digite sua altura em metros (ex: 1.73): "))
+peso   = float(input("Digite seu peso em kg (ex: 70): "))
 
+imc = peso / (altura ** 2)
+
+# Cabeçalho e contexto
+print(titulo)
+print(f" Peso: {peso}kg | Altura: {altura}m ".center(40, " "))
+print(separador)
+print("De acordo com a OMS:")
+
+# Apenas o resultado muda
+if imc < 18.5:
+    print("Abaixo do peso ideal.")
+elif imc < 25:
+    print("Peso ideal para sua altura.")
+elif imc < 30:
+    print("Sobrepeso.")
+else:
+    print("Obesidade.")
+
+print(footer)
--- mundo1/desafios_extras_mundo1/desafio_ex03_conversor_temperatura/desafio.py
+++ mundo1/desafios_extras_mundo1/desafio_ex03_conversor_temperatura/desafio.py
@@ -1,4 +1,3 @@
-
 # ============================================================
 # ! DESAFIO EX03 — Conversor de Temperatura
 # ============================================================
@@ -10,8 +9,17 @@
 # Descrição:
 # Converta Celsius para Fahrenheit e Kelvin.
 
-# * ============================================================
-# * ESCREVA SEU CÓDIGO ABAIXO
-# * ============================================================
+titulo = " Conversor de Temperatura 2.0 ".center(40, "\u2550")
+separador = "─" * 40
+footer = "\u2550" * 40
 
+fahrenheit = float(input("Digite a Temperatura: "))
+kelvin = ((fahrenheit - 32)/ 1.8) + 273.15
+
+print(f"\n{titulo}")
+print("Bem Vindo ao Conersor de Temperatura de Fº para Kº")
+print(separador)
+print(f"A temperatura de | {fahrenheit}Fº.")
+print(f"{'A temperarua em':<16} | {kelvin:.2f}Kº")
+print(footer)
 
