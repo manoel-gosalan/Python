e89202350341d1e9632db2ba7557a217f359d12c
Manoel Gosalan <manoel.gosalan@outlook.com>
2026-05-30 17:20:20 +0100

    feat(desafio_mundo2): add solved challenge 43

--- mundo2/Desafios/desafio_43_calculadora_IMC.py
+++ mundo2/Desafios/desafio_43_calculadora_IMC.py
@@ -25,4 +25,40 @@
 # Implemente a lógica completa do exercício.
 
 
+titulo = "Calculadora de IMC".center(40, "\u2550")
+separador = "─"*40
+fim = "\u2550" * 40
+
+nome = input("Informe o seu nome: ").capitalize()
+altura = float(input("Informe a sua Altura (ex 1.73): "))
+peso = float(input("Informe o seu peso (ex 103.03): "))
+IMC = peso / (altura ** 2 )
+txt_usuario = f"Olá \033[0m{nome}\033[1m bem vindo ao seu calculador de IMC."
+txt_imc = f"O seu IMC atual é de \033[0m{IMC:.2f}\033[1m de acordo com a OMS você está: "
+
+print(f"\n{titulo}")
+
+if altura < 0 or peso < 10:
+    print("Valores invalidos tente novamente.")
+
+# Calcula a classificação primeiro
+if IMC < 18.5:
+    classificacao = "Abaixo do peso ideal"
+elif IMC < 25.5:
+    classificacao = "No peso ideal"
+elif IMC < 30.0:
+    classificacao = "Sobrepeso. Comece uma academia!"
+elif IMC < 35.0:
+    classificacao = "Obesidade Grau 1. Dieta + academia."
+elif IMC < 40.0:
+    classificacao = "Obesidade Grau 2. Procure um personal e nutricionista."
+else:
+    classificacao = "\033[0mObesidade Grau 3 (MÓRBIDA)\033[1m. Procure um médico imediatamente."
+
+# Imprime UMA vez só
+print(txt_usuario)
+print(separador)
+print(txt_imc)
+print(classificacao)
+print(fim)
 
